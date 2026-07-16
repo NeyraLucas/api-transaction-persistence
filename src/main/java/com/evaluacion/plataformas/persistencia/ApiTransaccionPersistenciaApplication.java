f9d13204f73e0ac53a429d3b1c3f2f69ec3beedc
lui <luiscrazypeople@gmail.com>
2026-07-15 19:48:46 -0600

    first commit

---
+++ src/main/java/com/evaluacion/plataformas/persistencia/ApiTransaccionPersistenciaApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion.plataformas.persistencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTransaccionPersistenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTransaccionPersistenciaApplication.class, args);
+	}
+
+}
